04b98f041ae78ac03955c8d50f9781e11c695051
mehdi mohammadi <moh.meh41@gmail.com>
2021-07-15 18:34:42 +0430

    postgres

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,22 @@
-module.exports = {
-    //
-    graphql: {
-      endpoint: '/graphql',
-      shadowCRUD: true,
-      playgroundAlways: false,
-      depthLimit: 7,
-      amountLimit: 100,
-      apolloServer: {
-        tracing: false,
+module.exports = ({ env }) => {
+  // ...
+  if(env("NODE_ENV") === "production"){
+    return {
+      upload: {
+        provider: 'aws-s3',
+        providerOptions: {
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          region: env('AWS_REGION'),
+          params: {
+            Bucket: env('AWS_BUCKET'),
+          },
+        },
       },
-    },
-  };
+    }
+  }
+  return {
+
+  }
+  // ...
+};
