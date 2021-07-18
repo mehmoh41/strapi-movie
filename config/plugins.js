619c90c34a869962a24449e45b190064f1d7ee93
mehdi mohammadi <moh.meh41@gmail.com>
2021-07-18 14:01:38 +0430

    cloudinary added

--- config/plugins.js
+++ config/plugins.js
@@ -2,15 +2,23 @@ module.exports = ({ env }) => {
   // ...
   if(env("NODE_ENV") === "production"){
     return {
+      // upload: {
+      //   provider: 'aws-s3',
+      //   providerOptions: {
+      //     accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      //     secretAccessKey: env('AWS_ACCESS_SECRET'),
+      //     region: env('AWS_REGION'),
+      //     params: {
+      //       Bucket: env('AWS_BUCKET'),
+      //     },
+      //   },
+      // },
       upload: {
-        provider: 'aws-s3',
+        provider: 'cloudinary',
         providerOptions: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_ACCESS_SECRET'),
-          region: env('AWS_REGION'),
-          params: {
-            Bucket: env('AWS_BUCKET'),
-          },
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
         },
       },
     }
